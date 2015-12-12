3050933b975b36230fed1855528edc651c1c9529
Jack Bryant <jackrbryant@gmail.com>
2015-12-12 11:25:43 -0600

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Jack Randolph Bryant on 12/12/15.
+//  Copyright © 2015 Ellen Stanfil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
